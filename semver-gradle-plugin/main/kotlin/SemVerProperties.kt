adfd67731fb5d73b8d867c72bef0dcc90473aeb9
Javier Segovia Córdoba <javier@segoviacordoba.com>
2021-11-10 19:50:23 +0100

    Add support to set `semver` Gradle properties per project

--- semver-gradle-plugin/main/kotlin/SemVerProperties.kt
+++ semver-gradle-plugin/main/kotlin/SemVerProperties.kt
@@ -1,29 +1,33 @@
 package com.javiersc.semver.gradle.plugin
 
-import com.javiersc.semver.gradle.plugin.Scope.Patch
 import java.time.Instant
 import java.util.Date
 import org.gradle.api.Project
 
 internal val Project.stageProperty: String?
-    get() = properties[SemVerProperties.Stage.key]?.toString()
+    get() =
+        properties["$path:${SemVerProperties.Stage.key}"]?.toString()
+            ?: properties[SemVerProperties.Stage.key]?.toString()
 
 internal val Project.scopeProperty: String?
-    get() = properties[SemVerProperties.Scope.key]?.toString()
+    get() =
+        properties["$path:${SemVerProperties.Scope.key}"]?.toString()
+            ?: properties[SemVerProperties.Scope.key]?.toString()
 
 internal val Project.mockDate: Date?
     get() =
-        properties[SemVerProperties.MockDate.key]?.let { value ->
+        (properties["$path:${SemVerProperties.MockDate.key}"]?.toString()
+                ?: properties[SemVerProperties.MockDate.key])?.let { value ->
             checkNotNull(value.toString().toLongOrNull()) {
-                "`semver.mockDateOfEpochSecond` must be a number"
+                "`${SemVerProperties.MockDate.key}` must be a number"
             }
             Date.from(Instant.ofEpochSecond(value.toString().toLong()))
         }
 
-internal enum class SemVerProperties(val key: String, val value: String?) {
-    Stage("semver.stage", ""),
-    Scope("semver.scope", Patch.value),
-    MockDate("semver.mockDateOfEpochSecond", null),
+internal enum class SemVerProperties(val key: String) {
+    Stage("semver.stage"),
+    Scope("semver.scope"),
+    MockDate("semver.mockDateOfEpochSecond"),
 }
 
 internal enum class Scope(val value: String) {

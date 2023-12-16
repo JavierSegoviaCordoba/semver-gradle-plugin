5b20c29bc8b149b13bf8a1f5900ba26f045a30fd
Javier Segovia Córdoba <javiersegoviacordoba@gmail.com>
2023-12-16 15:36:01 +0100

    WIP refactor VersionTableTest

--- semver-project-gradle-plugin/test/kotlin/com/javiersc/semver/project/gradle/plugin/VersionsTableTest.kt
+++ semver-project-gradle-plugin/test/kotlin/com/javiersc/semver/project/gradle/plugin/VersionsTableTest.kt
@@ -64,6 +64,15 @@ internal class VersionTableTest {
 
     @ParameterizedTest
     @CsvFileSource(
+        resources = ["/tables/clean=true/scope=null stage=beta.csv"],
+        numLinesToSkip = 1,
+    )
+    fun `clean=true scope=null stage=beta`(@AggregateWith(Companion::class) table: VersionTable) {
+        table.checkTable()
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(
         resources = ["/tables/clean=true/scope=null stage=null.csv"],
         numLinesToSkip = 1,
     )
